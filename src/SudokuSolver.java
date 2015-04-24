24306ff485d6910bae520b7f30a1ecc7aae22c34
Bartleby <bartleby2718@gmail.com>
2015-04-23 22:19:45 -0400

    Introduced markpus
    1) Used markups for speed-up (20-second boost)
    2) Replaced unnecessary Integers with ints (shorter, less space)
    3) Added some comments

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -13,43 +13,46 @@ public class SudokuSolver {
 		int[][] c = new int[n][n];		
 		return null;
 	}*/
-	
-	public static Integer[][] solve(Integer[][] grid) {
-		ArrayList<Integer[][]> possibilities = new ArrayList<Integer[][]>();
+
+	public static int[][] solve(int[][] grid) {
+		ArrayList<int[][]> possibilities = new ArrayList<int[][]>();
 		possibilities.add(grid);
-        // TODO: Might change depending on the dimensions of the sukodu
+		// TODO: markup code
+		// int[][][] listOfMarkups = new int[9][9][9];
+		// TODO: Might change depending on the dimensions of the sukodu
 		for (int i = 0; i < 9; i++)
 			for (int j = 0; j < 9; j++)
                 if (!possibilities.isEmpty())
                     possibilities = generatePossibilities(possibilities, i, j);
         // If there is no solution or the given full sudoku is invalid
         if (possibilities.isEmpty() || !isValid(possibilities.get(0)))
-            return new Integer[grid.length][grid.length];
+            return new int[grid.length][grid.length];
         else
             return possibilities.get(0);
 	}
-	
-	static Integer[][] getSquare(Integer[][] grid, int i, int j) {
+
+	static int[][] getSquare(int[][] grid, int i, int j) {
         // TODO: Might change depending on the dimensions of the sukodu
-		Integer[][] square = new Integer[3][3];
+		int[][] square = new int[3][3];
 		for (int x = i; x < i + 3; x++)
 			for (int y = j; y < j + 3; y++)
 				square[x - i][y - j] = grid[x][y];
 		return square;
 	}
-	
-	static boolean checkSquare(Integer[][] square) {
+
+	// TODO: Maybe we should use listOfMarkups for this
+	static boolean checkSquare(int[][] square) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		for (int i = 0; i < square.length; i++)
 			for (int j = 0; j < square.length; j++) {
-				if (square[i][j] != 0 && list.contains(square[i][j]))
+				if (square[i][j] != 0 && list.contains(new Integer(square[i][j])))
 					return false;
 				list.add(square[i][j]);
 			}
 		return true;
 	}
-	
-	static boolean checkSquares(Integer[][] grid) {
+
+	static boolean checkSquares(int[][] grid) {
         // TODO: Might change depending on the dimensions of the sukodu
 		for (int i = 0; i < grid.length; i += 3)
 			for (int j = 0; j < grid.length; j += 3)
@@ -57,51 +60,58 @@ public class SudokuSolver {
 					return false;
 		return true;
 	}
-	
-	static boolean checkHorizontals(Integer[][] grid) {
+
+	// TODO: Maybe we should use listOfMarkups for this
+	static boolean checkHorizontals(int[][] grid) {
 		for (int i = 0; i < grid.length; i++) {
 			ArrayList<Integer> list = new ArrayList<Integer>();
 			for (int j = 0; j < grid.length; j++) {
-				if (grid[i][j] != 0 && list.contains(grid[i][j]))
+				if (grid[i][j] != 0 && list.contains(new Integer(grid[i][j])))
 					return false;
 				list.add(grid[i][j]);
-			}				
+			}
 		}
 		return true;
 	}
-	
-	static boolean checkVerticals(Integer[][] grid) {
+
+	// TODO: Maybe we should use listOfMarkups for this
+	static boolean checkVerticals(int[][] grid) {
 		for (int j = 0; j < grid.length; j++) {
 			ArrayList<Integer> list = new ArrayList<Integer>();
 			for (int i = 0; i < grid.length; i++) {
-				if (grid[i][j] != 0 && list.contains(grid[i][j]))
+				if (grid[i][j] != 0 && list.contains(new Integer(grid[i][j])))
 					return false;
 				list.add(grid[i][j]);
-			}				
+			}
 		}
 		return true;
 	}
-		
-	static boolean isValid(Integer[][] grid) {
+
+	static boolean isValid(int[][] grid) {
         return checkHorizontals(grid) && checkVerticals(grid) && checkSquares(grid);
 	}
-	
-	static ArrayList<Integer[][]> generatePossibilities(ArrayList<Integer[][]> current, int row, int col) {
+
+	static ArrayList<int[][]> generatePossibilities(ArrayList<int[][]> current, int row, int col) {
 		if (current.get(0)[row][col] != 0)
 			return current;
-		ArrayList<Integer[][]> next = new ArrayList<Integer[][]>();
-		Iterator<Integer[][]> iter = current.iterator();
+		// TODO: Might change depending on the dimensions of the sukodu
+		int[][][] listOfMarkups = new int[9][9][9];
+		ArrayList<int[][]> next = new ArrayList<int[][]>();
+		Iterator<int[][]> iter = current.iterator();
 		while (iter.hasNext()) {
-			Integer[][] grid = iter.next();
+			int[][] grid = iter.next();
 			iter.remove();
-            // TODO: Might change depending on the dimensions of the sukodu
-            // TODO: This outer for-loop have to replace with a markup later
-			for (int k = 1; k <= 9; k++) {
-				Integer[][] clone = new Integer[9][9];
+			Markup.updateListOfMarkups(grid, listOfMarkups);
+			// Don't try all numbers
+			for (int k : Markup.arrayToArrayList(listOfMarkups[row][col])) {
+				// Make a copy of the grid
+				int[][] clone = new int[grid.length][grid.length];
 				for (int i = 0; i < grid.length; i++)
 					for (int j = 0; j < grid.length; j++)
 						clone[i][j] = grid[i][j];
+				// Change only one entry
 				clone[row][col] = k;
+				// Add to "next" only if it's valid
 				if (isValid(clone))
 					next.add(clone);
 			}
@@ -113,7 +123,7 @@ public class SudokuSolver {
         // TODO: Might change depending on the dimensions of the sukodu
 		// If we were to change this, we should ask the width/length of the small square (3 for a regular sudoku).
 		// That way, we don't have to take square roots
-        Integer[][] grid = new Integer[9][9];
+        int[][] grid = new int[9][9];
 		Scanner fileScanner = new Scanner(new File("default.txt"));
         //Scanner fileScanner = new Scanner(new File("answer.txt"));
         int i = 0;
@@ -138,5 +148,5 @@ public class SudokuSolver {
 		frame.setVisible(true);
 		fileScanner.close();
 	}
-
 }
+
